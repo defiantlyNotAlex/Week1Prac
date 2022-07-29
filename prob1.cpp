a13e952e32a192b88876f2c34d2b07d3e1f88698
defiantlyNotAlex <104746120+defiantlyNotAlex@users.noreply.github.com>
2022-07-29 15:48:37 +0930

    Delete prob1.cpp

--- prob1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int array_sum(int array[], int n) {
-    int sum = 0;
-    for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
-        sum += array[i];
-    }
-
-    return sum;
-}
